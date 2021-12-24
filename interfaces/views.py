f85afaec7282f7fac0a9825a9d885c3945aa691d
jianyingfeng <jianyingfeng@codemao.cn>
2021-12-24 18:38:50 +0800

    12/24

--- interfaces/views.py
+++ interfaces/views.py
@@ -17,8 +17,17 @@ class InterfacesView(View):
         # 方式二：
         # 1）获取父表模型对象，再获取模型对象的id
         # 2）从表外键字段_id，传参为父表模型对象的id
-        projects_obj = Projects.objects.get(name='在线图书项目')
-        interfaces_obj = Interfaces.objects.create(name='在线图书项目-订单接口', tester='李莫愁', projects_id=projects_obj.id)
+        # projects_obj = Projects.objects.get(name='在线图书项目')
+        # interfaces_obj = Interfaces.objects.create(name='在线图书项目-订单接口', tester='李莫愁', projects_id=projects_obj.id)
+        # 查询接口名称包含‘登录’的项目数据
+        # 1）先按条件取到接口表数据，再通过外键属性查询父表数据
+        # Interfaces.objects.filter(name__contains='登录').first().projects
+        # 查询项目负责人包含‘热水’的接口数据
+        # 方式一：从子表查到父表
+        # Interfaces.objects.filter(projects__leader__contains='热水')
+        # 方式二：
+        # 从父表查到字表
+        # Projects.objects.filter(leader__contains='热水').first().interfaces_set.all()
         pass
 
     def post(self, request, pk):
