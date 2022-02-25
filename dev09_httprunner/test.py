fd4e278e69576bbc9490d1bb46459731cb2622ed
jianyingfeng <jianyingfeng@codemao.cn>
2022-02-25 19:15:03 +0800

    02/25

--- dev09_httprunner/test.py
+++ dev09_httprunner/test.py
@@ -2,6 +2,6 @@ from httprunner.api import HttpRunner
 
 httprun = HttpRunner()
 # 传入文件绝对路径，再运行
-httprun.run(r'C:\Users\Jyf\PycharmProjects\dev10\dev09_httprunner\api\login_api.yml')
+httprun.run(r'C:\Users\jianyingfeng\PycharmProjects\dev09\dev09_httprunner\api\login_api.yml')
 # 打印日志信息
 print(httprun._summary)
